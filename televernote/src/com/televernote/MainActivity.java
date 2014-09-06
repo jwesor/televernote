4d9256117b5b02d2ef605a21dd3f93acf954ebbd
ericandoh <ohjingyu@gmail.com>
2014-09-06 11:09:08 -0700

    Added in evernote API

--- televernote/src/com/televernote/MainActivity.java
+++ televernote/src/com/televernote/MainActivity.java
@@ -1,5 +1,7 @@
 package com.televernote;
 
+import com.evernote.client.android.EvernoteSession;
+
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -7,10 +9,30 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+	// Your Evernote API key. See http://dev.evernote.com/documentation/cloud/
+	// Please obfuscate your code to help keep these values secret.
+	private static final String CONSUMER_KEY = "eric5";
+	private static final String CONSUMER_SECRET = "c286186b14af5124";
+	
+	// Initial development is done on Evernote's testing service, the sandbox.
+	// Change to HOST_PRODUCTION to use the Evernote production service
+	// once your code is complete, or HOST_CHINA to use the Yinxiang Biji
+	// (Evernote China) production service.
+	private static final EvernoteSession.EvernoteService EVERNOTE_SERVICE = EvernoteSession.EvernoteService.SANDBOX;
+
+	protected EvernoteSession mEvernoteSession;
+
+	// Set this to true if you want to allow linked notebooks for accounts that can only access a single
+	// notebook.
+	private static final boolean SUPPORT_APP_LINKED_NOTEBOOKS = true;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+	    //Set up the Evernote Singleton Session
+	    mEvernoteSession = EvernoteSession.getInstance(this, CONSUMER_KEY, CONSUMER_SECRET, EVERNOTE_SERVICE, SUPPORT_APP_LINKED_NOTEBOOKS);
 	}
 
 	@Override
