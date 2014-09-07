f8ca603fb4b037cf22f62037f2a9011696438125
Jon Wang <jwesor@gmail.com>
2014-09-06 19:25:31 -0700

    Added ReceiverActivity timestamp string parsing

--- televernote/src/com/televernote/telegraph/ReceiverActivity.java
+++ televernote/src/com/televernote/telegraph/ReceiverActivity.java
@@ -1,5 +1,7 @@
 package com.televernote.telegraph;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 
 import com.televernote.R;
+import com.televernote.morse.Transcriber;
 
 /**
  * Activity for displaying received messages.
@@ -18,7 +21,7 @@ public class ReceiverActivity extends TelegraphActivity {
 	private static final int INITIAL_DELAY = 1000;
 	public static String SENDER_KEY = "sender";
 
-	private long[] times;
+	private List<Long> times;
 	private int index;
 	private String sender;
 
@@ -26,7 +29,19 @@ public class ReceiverActivity extends TelegraphActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Bundle bundle = getIntent().getExtras();
-		times = bundle.getLongArray("timestamps");
+		String data = bundle.getString("timestamps");
+		times = new ArrayList<Long>();
+		String[] timestamps = data.split(Transcriber.DELIM);
+		for (String ts: timestamps) {
+			if (ts.length() > 0) {
+				try {
+					times.add(Long.parseLong(ts));
+				} catch (NumberFormatException e) {
+					System.out.println("Improperly formatted timestamp " + ts);
+				}
+			}
+		}
+
 		sender = bundle.getString(SENDER_KEY);
 		updateText();
 		index = 0;
@@ -43,7 +58,7 @@ public class ReceiverActivity extends TelegraphActivity {
 	protected void updateText() {
 		String header = getString(R.string.message_sender);
 		header = String.format(header, sender.toUpperCase(Locale.US));
-		text.setText(header + "\n" + decoder.getTranslatedMorseBuffer(index >= times.length - 1));
+		text.setText(header + "\n" + decoder.getTranslatedMorseBuffer(index >= times.size() - 1));
 	}
 
 	private void initialTap() {
@@ -54,10 +69,10 @@ public class ReceiverActivity extends TelegraphActivity {
 
 	private void timeNextTap() {
 		index ++;
-		if (index >= times.length) {
+		if (index >= times.size()) {
 			button.setEnabled(true);
 		} else {
-			CountDownTimer timer = new TapTimer(times[index] - times[index - 1], index % 2 == 0);
+			CountDownTimer timer = new TapTimer(times.get(index) - times.get(index-1), index % 2 == 0);
 			timer.start();
 		}
 	}
